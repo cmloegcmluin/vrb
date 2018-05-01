adf70c218d3c741f3a20adc7c1ae6bb81425c4ec
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-05-01 12:59:10 +0900

    slow down the rotation from the mouse controls in orthographic view

--- src/buildMouseControls.js
+++ src/buildMouseControls.js
@@ -3,7 +3,8 @@ import {OrbitControls} from 'three-full'
 const buildMouseControls = ({renderer, cameras}) => {
     const mouseControls = new OrbitControls(cameras.orthographicCamera, renderer.domElement)
     mouseControls.enableDamping = true
-    mouseControls.dampingFactor = .1
+    mouseControls.dampingFactor = 0.1
+    mouseControls.rotateSpeed = 0.1
 
     return mouseControls
 }
