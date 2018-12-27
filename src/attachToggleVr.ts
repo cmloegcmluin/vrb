7489541469d3094826d740f86cde053a95c33677
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-27 14:14:48 -0800

    make chrome happy with xr i hope

--- src/attachToggleVr.ts
+++ src/attachToggleVr.ts
@@ -3,29 +3,40 @@ import { AttachToggleVrParameters } from './types'
 const attachToggleVr = async ({ cameras, renderer, toggle, mouseControls }: AttachToggleVrParameters): Promise<void> => {
     const toggleVr = () => cameras.currentCamera === cameras.perspectiveCamera ? exitPresent() : enterPresent()
 
-    // @ts-ignore
-    let device
+    let device: VRDisplay
     // @ts-ignore
     if (navigator.xr) {
         // @ts-ignore
         device = await navigator.xr.requestDevice()
-        await device.supportsSession( { immersive: true, exclusive: true /* DEPRECATED */ } )
     } else {
-        const displays = await navigator.getVRDisplays()
-        device = displays[0]
+        const displays: VRDisplay[] = await navigator.getVRDisplays()
+        device = displays[ 0 ]
     }
     renderer.vr.setDevice(device)
 
-    const exitPresent = () => {
+    const exitPresent = async () => {
         // @ts-ignore
-        device.exitPresent()
+        if (navigator.xr) {
+            // @ts-ignore
+            renderer.vr.setSession(null)
+        } else {
+            await device.exitPresent()
+        }
         mouseControls.enabled = true
         cameras.currentCamera = cameras.orthographicCamera
     }
 
-    const enterPresent = () => {
+    const enterPresent = async () => {
         // @ts-ignore
-        device.requestPresent([ { source: renderer.domElement } ])
+        if (navigator.xr) {
+            // @ts-ignore
+            const session = await device.requestSession({ immersive: true, exclusive: true })
+            // @ts-ignore
+            renderer.vr.setSession(session)
+        } else {
+            await device.requestPresent([ { source: renderer.domElement } ])
+        }
+
         mouseControls.enabled = false
         cameras.currentCamera = cameras.perspectiveCamera
     }
